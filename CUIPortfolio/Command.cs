8efa3636f72e2c2100f228623f8fabf95b54f916
Atria <atriasft@gmail.com>
2020-09-20 16:54:45 +0900

    Create Command.cs

---
+++ CUIPortfolio/Command.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CUIPortfolio
+{
+    public class Command
+    {
+        public Command(string name, string explanation)
+        {
+            Name = name;
+            Explanation = explanation;
+        }
+
+        //コマンド名
+        public string Name { get; private set; }
+        //コマンドの動作内容
+        public string Explanation { get; private set; }
+    }
+}
