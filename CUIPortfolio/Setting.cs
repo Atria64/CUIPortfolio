9f23b10415382283538cebb0ccccd1ff13450e5f
Atria <atriasft@gmail.com>
2020-09-22 22:38:31 +0900

    changecolorでNameや$マークの色を変更できるようにした

--- CUIPortfolio/Setting.cs
+++ CUIPortfolio/Setting.cs
@@ -3,5 +3,7 @@
     static public class Setting
     {
         public static string CommandColor { get; set; } = "skyblue";
+        public static string NameColor { get; set; } = "gold";
+        public static string DollarMarkColor { get; set; } = "red";
     }
 }
